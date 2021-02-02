5e1d813d960a5a4cf433b69886e0fdb6612f496d
fredrik-luckan <60471253+fredrik-luckan@users.noreply.github.com>
2021-02-02 12:39:22 +0200

    Update archive.php

--- archive.php
+++ archive.php
@@ -16,12 +16,12 @@ $description = get_the_archive_description();
 
 <?php if ( have_posts() ) : ?>
 
-	<header class="page-header alignwide">
+	<!--<header class="page-header alignwide">
 		<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
 		<?php if ( $description ) : ?>
 			<div class="archive-description"><?php echo wp_kses_post( wpautop( $description ) ); ?></div>
 		<?php endif; ?>
-	</header><!-- .page-header -->
+	</header> --> <!-- .page-header -->
 
 	<?php while ( have_posts() ) : ?>
 		<?php the_post(); ?>
