1f625c977e43d9b9d2faf92c279f121c6fcc7199
fredrik-luckan <60471253+fredrik-luckan@users.noreply.github.com>
2021-04-22 15:15:28 +0300

    Update site-header.php

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -19,3 +19,5 @@ $wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
 	<?php get_template_part( 'template-parts/header/site-nav' ); ?>
 
 </header><!-- #masthead -->
+
+<hr class="divider">
