f0da00fd549684264a498102e9f7ad3a9f2f7630
fredrik-luckan <60471253+fredrik-luckan@users.noreply.github.com>
2021-06-29 14:49:07 +0300

    Create archive-kunskapsbanker.php

---
+++ archive-kunskapsbanker.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+$description = get_the_archive_description();
+?>
+
+<?php if ( have_posts() ) : ?>
+
+	<?php while ( have_posts() ) : ?>
+		<?php the_post(); ?>
+		<?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
+	<?php endwhile; ?>
+
+
+<?php else : ?>
+	<?php get_template_part( 'template-parts/content/content-none' ); ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
